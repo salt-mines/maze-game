3cdb7df97c84e936fae329a57210a32b7d3dd9ac
pasimakit <48199458+pasimakit@users.noreply.github.com>
2019-06-24 14:59:37 +0300

    fixed walls, camera moves and goal

--- Assets/Scripts/LightScript.cs
+++ Assets/Scripts/LightScript.cs
@@ -7,22 +7,31 @@ public class LightScript : MonoBehaviour
     private Light lightSource;
 
     public float depleteAmount = 0.01f;
+    public float maxRange = 5f;
+    public float minRange = 2.5f;
 
     // Start is called before the first frame update
     void Start()
     {
         lightSource = GetComponent<Light>();
         lightSource.intensity = 1;
+        lightSource.range = maxRange;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(lightSource.range > minRange)
+        {
             lightSource.intensity -= depleteAmount * Time.deltaTime;
+            lightSource.range -= depleteAmount * Time.deltaTime;
+        }
+        
     }
 
-    void powerPickup()
+    public void powerPickup()
     {
         lightSource.intensity = 1;
+        lightSource.range = maxRange;
     }
 }
